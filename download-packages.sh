890aa802db6e2efa2eb355509dcadc03018d8c61
Vlad Temian <vladtemian@gmail.com>
2025-08-13 14:31:20 +0300

    gitfs: vendor urllib3 sdist for offline pex resolution (sentry-sdk dep); update VERSIONS and downloader; include in source

--- download-packages.sh
+++ download-packages.sh
@@ -27,6 +27,7 @@ wget -q "$(get_var MFUSEPY_URL)" -O "debian-gitfs/packages/mfusepy-$(get_var MFU
 wget -q "$(get_var PYCPARSER_URL)" -O "debian-gitfs/packages/pycparser-$(get_var PYCPARSER_VERSION).tar.gz"
 wget -q "$(get_var PYGIT2_URL)" -O "debian-gitfs/packages/pygit2-$(get_var PYGIT2_VERSION).tar.gz"
 wget -q "$(get_var SENTRY_SDK_URL)" -O "debian-gitfs/packages/sentry-sdk-$(get_var SENTRY_SDK_VERSION).tar.gz"
+wget -q "$(get_var URLLIB3_URL)" -O "debian-gitfs/packages/urllib3-$(get_var URLLIB3_VERSION).tar.gz"
 wget -q "$(get_var PIP_URL)" -O "debian-gitfs/packages/pip-$(get_var PIP_VERSION).tar.gz"
 wget -q "$(get_var SETUPTOOLS_URL)" -O "debian-gitfs/packages/setuptools-$(get_var SETUPTOOLS_VERSION).tar.gz"
 wget -q "$(get_var WHEEL_URL)" -O "debian-gitfs/packages/wheel-$(get_var WHEEL_VERSION).tar.gz"
